a5556c085d9c5fe6c3da518e73b8ae73a3a93285
MarcosX <markito5_net@hotmail.com>
2025-01-03 14:18:32 -0300

    pj update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // Specify the output directory
-    sourcemap: true, // Generate source maps for debugging
-    minify: 'esbuild', // Use esbuild for minification
-  },
+  plugins: [react()]
 })
